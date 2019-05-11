4181cb1a85e132d09bdb88b896ed1067b762621e
Elemigrante <gasdom@yandex.ru>
2019-05-11 15:51:21 +0300

    test to validate the uniqueness of the question text

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Question, type: :model do
     # Проверяем, разрешено ли поле со значением 14
     it { should allow_value(14).for(:level) }
     it { should_not allow_value(15).for(:level) }
+    
+    # Проверяем на валидацию уникальности текста вопроса
+    # Явно создаем "предмет тестирования" - валидный объект
+    subject { Question.new(text: 'some',
+                           level: 0, answer1: '1', answer2: '1', answer3: '1', answer4: '1') }
+    # Только с валидным объектом работает этот тест,
+    # Иначе пытается создать в базе новый невалидный
+    it { should validate_uniqueness_of(:text) }
   end
 end
