6d0555e9a91cc70d42483e4a5778fd91bce1c4d8
Elemigrante <gasdom@yandex.ru>
2019-05-15 11:44:11 +0300

    test for GamesController#take_money

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -76,5 +76,22 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(root_path)
       expect(flash[:alert]).to be # во flash должен быть прописана ошибка
     end
+    # Проверка ситуации, когда пользователь берет деньги до конца игры
+    it 'take money before end game' do
+      # вручную поднимем уровень вопроса до выигрыша 200
+      game_w_questions.update_attribute(:current_level, 2)
+  
+      put :take_money, id: game_w_questions.id
+      game = assigns(:game)
+      expect(game.finished?).to be_truthy
+      expect(game.prize).to eq(200)
+  
+      # пользователь изменился в базе, надо в коде перезагрузить!
+      user.reload
+      expect(user.balance).to eq(200)
+  
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:warning]).to be
+    end
   end
 end
