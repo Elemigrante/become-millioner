cea12b2c6db8635af76e0825477cfe124e4e47a4
Elemigrante <gasdom@yandex.ru>
2019-05-29 14:04:42 +0300

    updated feature test for User viewing someones profile

--- spec/features/user_viewing_someones_profile_spec.rb
+++ spec/features/user_viewing_someones_profile_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.feature 'USER viewing someones profile', type: :feature do
   # Создаём объект пользователь в базе для теста
-  let(:user1) { FactoryBot.create :user, name: 'Grognak' }
-  let(:user2) { FactoryBot.create :user, name: 'Konan' }
+  let(:user1) { FactoryBot.create :user, id: 1, name: 'Grognak' }
+  let(:user2) { FactoryBot.create :user, id: 2, name: 'Konan' }
   
   # А также объект игры
   let!(:games) do
     [
       FactoryBot.create(:game,
-                        id: 11,
+                        id: 1,
                         user: user2,
                         created_at: Time.parse('2019.05.09, 13:00'),
-                        current_level: 2,
-                        prize: 1000),
-      
+                        prize: 1000
+      ),
+
       FactoryBot.create(:game,
-                        id: 12,
+                        id: 2,
                         user: user2,
-                        created_at: Time.parse('2019.05.09, 15:00'),
-                        current_level: 4,
-                        prize: 4000)
-    ]
+                        created_at: Time.parse('2019.05.09, 14:00'),
+                        prize: 2000
+      )
+     ]
   end
   
   # Перед началом сценария, авторизовываем пользователя
@@ -38,27 +38,17 @@ RSpec.feature 'USER viewing someones profile', type: :feature do
     click_link 'Konan'
     
     # Ожидаем, что попадем на нужный url
-    expect(page).to have_current_path("/users/1")
+    expect(page).to have_current_path("/users/2")
+    
+    # Имя пользователя
+    expect(page).to have_content('Konan')
     
     # Ожидаем, что на экране не будет ссылки на смену пароля
     expect(page).not_to have_content('Сменить имя и пароль')
     
     # Ожидаем, что на экране будет:
-    #
-    # Имя пользователя
-    expect(page).to have_content('Konan')
-    # id игры
-    expect(page).to have_content('11')
-    # Дата создания игры
-    expect(page).to have_content('09 мая, 13:00')
-    # Уровень игры
-    expect(page).to have_content('2')
-    # Приз за игру
-    expect(page).to have_content('1 000')
+    expect(page).to have_content('1 в процессе 09 мая, 13:00 0 1 000 ₽ 50/50')
+    expect(page).to have_content('2 в процессе 09 мая, 14:00 0 2 000 ₽ 50/50')
 
-    expect(page).to have_content('12')
-    expect(page).to have_content('09 мая, 15:00')
-    expect(page).to have_content('4')
-    expect(page).to have_content('4 000')
   end
 end
